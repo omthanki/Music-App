fb168562653282597b3c93b7ead7e90bd4a43c70
Om Thanki <othanki874@rku.ac.in>
2023-04-09 22:29:28 +0530

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/NavBar.css";
+
+const NavBar = () => {
+    return(
+        <div className="navbar">
+            <i className="fab fa-spotify"></i>
+            <div className="app-header">Spotify Clone</div>
+        </div>
+    );
+};
+
+export default NavBar;
